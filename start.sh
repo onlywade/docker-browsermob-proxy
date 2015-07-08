5e4c45ebff78353631ca6c7adf4416a2474af1e0
wade <onlywade@gmail.com>
2015-07-08 13:41:13 -0700

    Adding an easy way to set an upstream proxy when starting the container.

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Give server time to boot, then setup proxy on port 9091
-(sleep 3 && curl -sSX POST -d "port=9091&bindAddress=$(cat /etc/hosts | grep $(hostname) | cut -f1)" http://0.0.0.0:9090/proxy) &
+if [[ ! -z $UPSTREAM_PROXY ]]; then
+    UPSTREAM_PARAMS="&httpProxy=$UPSTREAM_PROXY&httpsProxy=$UPSTREAM_PROXY"
+fi
+(sleep 3 && curl -sSX POST -d "port=9091&bindAddress=$(cat /etc/hosts | grep $(hostname) | cut -f1)${UPSTREAM_PARAMS}" http://0.0.0.0:9090/proxy) &
 
 exec /browsermob-proxy-2.0-beta-9/bin/browsermob-proxy -port 9090
